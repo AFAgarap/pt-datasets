2ea05829461b24ea7d1b2c156c5f65ea1b51308e
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-11-03 01:18:00 +0800

    chore: add package setup

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+with open("requirements.txt", "r") as file:
+    requirements = file.read().splitlines()
+
+setup(
+    name="pt-datasets",
+    version="0.1.0",
+    packages=["pt_datasets"],
+    url="https://github.com/AFAgarap/pt-datasets",
+    license="AGPL-3.0 License",
+    author="Abien Fred Agarap",
+    author_email="abienfred.agarap@gmail.com",
+    description="PyTorch dataset loader for MNIST, Fashion-MNIST, EMNIST-Balanced, CIFAR10, and SVHN datasets",
+    install_requires=requirements,
+)
