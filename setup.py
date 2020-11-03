bc8712caf9c3ae5c6ab74d2faa6234064f3c5be2
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-11-03 22:13:08 +0800

    fix: add list of dependencies in setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,22 @@
 from setuptools import setup
 
-with open("requirements.txt", "r") as file:
-    requirements = file.read().splitlines()
 
 setup(
     name="pt-datasets",
-    version="0.1.1",
+    version="0.1.2",
     packages=["pt_datasets"],
     url="https://github.com/AFAgarap/pt-datasets",
     license="AGPL-3.0 License",
     author="Abien Fred Agarap",
     author_email="abienfred.agarap@gmail.com",
     long_description="PyTorch dataset loader for MNIST, Fashion-MNIST, EMNIST-Balanced, CIFAR10, and SVHN datasets",
-    install_requires=requirements,
+    install_requires=[
+        "numpy==1.19.0",
+        "torchvision==0.7.0",
+        "umap_learn==0.4.6",
+        "torch==1.6.0",
+        "MulticoreTSNE==0.1",
+        "scikit_learn==0.23.2",
+        "umap==0.1.1",
+    ],
 )
