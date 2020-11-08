bc44fc91e47d7c176c00ff59a5f1fdad79223f81
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-11-08 21:18:52 +0800

    chore: update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup
 
 setup(
     name="pt-datasets",
-    version="0.1.3",
+    version="0.2.0",
     packages=["pt_datasets"],
     url="https://github.com/AFAgarap/pt-datasets",
     license="AGPL-3.0 License",
     author="Abien Fred Agarap",
     author_email="abienfred.agarap@gmail.com",
-    long_description="PyTorch dataset loader for MNIST, Fashion-MNIST, EMNIST-Balanced, CIFAR10, and SVHN datasets",
+    description="PyTorch dataset loader for MNIST, Fashion-MNIST, EMNIST-Balanced, CIFAR10, SVHN, and MalImg datasets",
+    long_description="PyTorch dataset loader for MNIST, Fashion-MNIST, EMNIST-Balanced, CIFAR10, SVHN, and MalImg datasets",
     install_requires=[
         "numpy==1.19.0",
         "torchvision==0.7.0",
@@ -17,5 +18,6 @@ setup(
         "torch==1.6.0",
         "MulticoreTSNE==0.1",
         "scikit_learn==0.23.2",
+        "gdown==3.12.2",
     ],
 )
