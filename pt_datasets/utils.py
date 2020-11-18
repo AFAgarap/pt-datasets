682e2e13b945057a95cfae89359a1161659e5234
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-11-18 18:06:10 +0800

    add docstring for AG News file reader

--- pt_datasets/utils.py
+++ pt_datasets/utils.py
@@ -20,6 +20,22 @@ __author__ = "Abien Fred Agarap"
 
 
 def read_data(corpus_file: str, label_column: int = 0, document_start: int = 2) -> Dict:
+    """
+    Returns a <key, value> pair of the loaded dataset
+    where the key is the text data and the value is the data label.
+    Parameters
+    ----------
+    corpus_file: str
+        The filename of the dataset to load.
+    label_column: int
+        The column number of the dataset label (zero-indexed).
+    document_start: int
+        The number of columns in the dataset.
+    Returns
+    -------
+    dataset: Dict
+        The <key, value> pair representing the text data and their labels.
+    """
     dataset = dict()
     with open(corpus_file, "r", encoding="utf-8") as text_data:
         if corpus_file.endswith(".csv"):
