3e1eb08ed79f40b31941368eeebb6725dcaaa4f1
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-11-26 23:25:35 +0800

    feat: perform z-score normalization on cifar10

--- pt_datasets/load_dataset.py
+++ pt_datasets/load_dataset.py
@@ -105,6 +105,14 @@ def load_dataset(
             transform=transform,
         )
     elif name == "cifar10":
+        transform = torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize(
+                    (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+                ),
+            ]
+        )
         train_dataset = torchvision.datasets.CIFAR10(
             root=data_folder, train=True, download=True, transform=transform
         )
