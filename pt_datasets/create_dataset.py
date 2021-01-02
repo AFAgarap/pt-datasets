5abb48e4e7e708c0f8aefefe524a90707223ff26
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-01-02 23:14:27 +0800

    add function docstring

--- pt_datasets/create_dataset.py
+++ pt_datasets/create_dataset.py
@@ -4,5 +4,21 @@ import torch
 def create_dataset(
     features: torch.Tensor, labels: torch.Tensor
 ) -> torch.utils.data.TensorDataset:
+    """
+    Returns a torch.utils.data.TensorDataset object
+    to be used for a data loader object.
+
+    Parameters
+    ----------
+    features: torch.Tensor
+        The input features.
+    labels: torch.Tensor
+        The input labels.
+
+    Returns
+    -------
+    dataset: torch.utils.data.TensorDataset
+        The Tensor Dataset object.
+    """
     dataset = torch.utils.data.TensorDataset(features, labels)
     return dataset
