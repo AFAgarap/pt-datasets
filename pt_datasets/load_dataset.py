f369ee46c775c34ccef82992b46d1025844862f9
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-01-07 22:40:16 +0800

    add conditional for normalizing CIFAR10

--- pt_datasets/load_dataset.py
+++ pt_datasets/load_dataset.py
@@ -124,14 +124,15 @@ def load_dataset(
             transform=transform,
         )
     elif name == "cifar10":
-        transform = torchvision.transforms.Compose(
-            [
-                torchvision.transforms.ToTensor(),
-                torchvision.transforms.Normalize(
-                    (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
-                ),
-            ]
-        )
+        if normalize:
+            transform = torchvision.transforms.Compose(
+                [
+                    torchvision.transforms.ToTensor(),
+                    torchvision.transforms.Normalize(
+                        (0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)
+                    ),
+                ]
+            )
         train_dataset = torchvision.datasets.CIFAR10(
             root=data_folder, train=True, download=True, transform=transform
         )
