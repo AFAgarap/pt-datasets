aa27057a55f7427327bbb4f4a59ad2b078bcbbf7
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-01-08 20:56:34 +0800

    shuffle data

--- pt_datasets/load_dataset.py
+++ pt_datasets/load_dataset.py
@@ -339,7 +339,7 @@ def load_wdbc(test_size: float = 3e-1, seed: int = 42):
     """
     features, labels = load_breast_cancer(return_X_y=True)
     train_features, test_features, train_labels, test_labels = train_test_split(
-        features, labels, test_size=test_size, random_state=seed
+        features, labels, test_size=test_size, random_state=seed, shuffle=True
     )
     train_dataset = torch.utils.data.TensorDataset(
         torch.from_numpy(train_features), torch.from_numpy(train_labels)
