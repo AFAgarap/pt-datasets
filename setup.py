f3f2b01171ea33f47bbd915b8788451bacc525ae
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-01-09 22:31:21 +0800

    chore: increment package version

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ def _post_install():
 
 setup(
     name="pt-datasets",
-    version="0.7.0",
+    version="0.7.1",
     packages=["pt_datasets"],
     url="https://github.com/AFAgarap/pt-datasets",
     license="AGPL-3.0 License",
