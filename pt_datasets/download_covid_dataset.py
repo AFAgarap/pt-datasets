660296f28f22e76ef6683ce9071ec1869131ea5d
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-01-20 16:28:30 +0800

    fix filename and vars

--- pt_datasets/download_covid_dataset.py
+++ pt_datasets/download_covid_dataset.py
@@ -4,15 +4,15 @@ from pathlib import Path
 import gdown
 
 
-BINARY_COVID_DATASET = (
+BINARY_COVID19_DATASET = (
     "https://drive.google.com/uc?id=1rcYxrMmxtX3JSW9eHT20sTzmRHWrmGc2"
 )
 
 
-def download_binary_covid_dataset():
+def download_binary_covid19_dataset():
     """
     Downloads the COVID19 Binary classification dataset
     """
     path = os.path.join(str(Path.home()), "datasets")
-    filename = "BinaryCOVIDDataset.zip"
-    gdown.download(BINARY_COVID_DATASET, os.path.join(path, filename))
+    filename = "BinaryCOVID19Dataset.zip"
+    gdown.download(BINARY_COVID19_DATASET, os.path.join(path, filename))
