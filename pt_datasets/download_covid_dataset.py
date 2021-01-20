6bb2f45a4a5bfbaf999ce5929e9a50cdc510df7a
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-01-20 16:13:40 +0800

    define function for downloading binary classification dataset for COVID19

---
+++ pt_datasets/download_covid_dataset.py
@@ -0,0 +1,15 @@
+import os
+from pathlib import Path
+
+import gdown
+
+
+BINARY_COVID_DATASET = (
+    "https://drive.google.com/uc?id=1rcYxrMmxtX3JSW9eHT20sTzmRHWrmGc2"
+)
+
+
+def download_binary_covid_dataset():
+    path = os.path.join(str(Path.home()), "datasets")
+    filename = "BinaryCOVIDDataset.zip"
+    gdown.download(BINARY_COVID_DATASET, os.path.join(path, filename))
