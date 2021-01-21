07f4f106dfb176a5456fbe03fb3057fe02bb106b
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-01-21 19:05:57 +0800

    add new link to dataset

--- pt_datasets/download_covid_dataset.py
+++ pt_datasets/download_covid_dataset.py
@@ -5,7 +5,7 @@ import gdown
 
 
 BINARY_COVID19_DATASET = (
-    "https://drive.google.com/uc?id=1rcYxrMmxtX3JSW9eHT20sTzmRHWrmGc2"
+    "https://drive.google.com/uc?id=1sGHrm76Rt_qRoHo-nU3A4iK8RMuTRMB6"
 )
 
 
@@ -13,6 +13,7 @@ def download_binary_covid19_dataset():
     """
     Downloads the COVID19 Binary classification dataset
     """
-    path = os.path.join(str(Path.home()), "torch_datasets")
-    filename = "BinaryCOVID19Dataset.zip"
+    path = os.path.join(str(Path.home()), "datasets")
+    filename = "BinaryCOVID19Dataset.tar.xz"
+    print("[INFO] Downloading the dataset...")
     gdown.download(BINARY_COVID19_DATASET, os.path.join(path, filename))
