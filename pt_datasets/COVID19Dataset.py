4716243eda7fe248de8181094ea3ec802026e107
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-01-21 23:50:41 +0800

    add license header

--- pt_datasets/COVID19Dataset.py
+++ pt_datasets/COVID19Dataset.py
@@ -1,3 +1,19 @@
+# PyTorch Datasets utility repository
+# Copyright (C) 2020  Abien Fred Agarap
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""COVID19 dataset classes"""
 import os
 from pathlib import Path
 from typing import Dict
@@ -5,6 +21,8 @@ from typing import Dict
 import torch
 
 from pt_datasets.utils import read_metadata, load_image
+
+__author__ = "Abien Fred Agarap"
 
 
 DATASET_DIR = os.path.join(str(Path.home()), "torch_datasets/BinaryCOVID19Dataset")
