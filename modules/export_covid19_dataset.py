628ca7a11e3358364f90ebe1fbac67b8e0080e92
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-01-23 20:38:55 +0800

    add argument parser

--- modules/export_covid19_dataset.py
+++ modules/export_covid19_dataset.py
@@ -1,3 +1,5 @@
+import argparse
+import os
 import time
 from typing import List, Tuple
 
@@ -5,6 +7,44 @@ import numpy as np
 import torch
 
 from pt_datasets import load_dataset, create_dataloader
+
+
+__author__ = "Abien Fred Agarap"
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="COVID19 dataset exporter")
+    group = parser.add_argument_group("Parameters")
+    group.add_argument(
+        "-d",
+        "--dataset",
+        type=str,
+        default="multi_covid",
+        help="the COVID19 dataset to export, options: [binary_covd | multi_covid (default)]",
+    )
+    group.add_argument(
+        "-b",
+        "--batch_size",
+        type=int,
+        default=2048,
+        help="the mini-batch size to use, default: [2048]",
+    )
+    group.add_argument(
+        "-f",
+        "--filename",
+        type=str,
+        default="torch_datasets/MultiCOVID19Dataset",
+        help="the path where the datasets shall be exported.",
+    )
+    group.add_argument(
+        "-s",
+        "--size",
+        type=int,
+        default=64,
+        help="the size to use for resizing the COVID19 data, default: [64]",
+    )
+    arguments = parser.parse_args()
+    return arguments
 
 
 def unpack_examples(data_loader: torch.utils.data.Dataset) -> Tuple[List, List]:
@@ -39,8 +79,9 @@ def export_dataset(dataset: np.ndarray, filename: str) -> None:
     torch.save(dataset, filename)
 
 
-def main():
-    batch_size = 2048
+def main(arguments):
+    batch_size = arguments.batch_size
+    path = arguments.filename
     train_data, test_data = load_dataset("multi_covid")
     train_loader = create_dataloader(train_data, batch_size=batch_size)
     test_loader = create_dataloader(test_data, batch_size=len(test_data))
@@ -52,14 +93,15 @@ def main():
         batch_size=batch_size,
     )
     train_dataset = (train_features, train_labels)
-    export_dataset(train_dataset, "train.pt")
+    export_dataset(train_dataset, os.path.join(path, "train.pt"))
     test_features, test_labels = unpack_examples(test_loader)
     test_features, test_labels = vectorize_examples(
         test_features, test_labels, dataset_size=len(test_data), batch_size=batch_size
     )
     test_dataset = (test_features, test_labels)
-    export_dataset(test_dataset, "test.pt")
+    export_dataset(test_dataset, os.path.join(path, "test.pt"))
 
 
 if __name__ == "__main__":
-    main()
+    arguments = parse_args()
+    main(arguments)
