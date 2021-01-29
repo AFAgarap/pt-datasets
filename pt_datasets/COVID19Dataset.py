c56f5209c9f1536e111d9a7ea6385fb717efdfa9
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-01-29 21:17:06 +0800

    load training and test sets

--- pt_datasets/COVID19Dataset.py
+++ pt_datasets/COVID19Dataset.py
@@ -205,6 +205,14 @@ class MultiCOVID19Dataset(torch.utils.data.Dataset):
             if not os.path.isfile(os.path.join(MULTI_COVID19_DIR, f"test_{size}.pt")):
                 print("[INFO] No preprocessed test dataset found. Preprocessing now...")
                 preprocess_dataset(train=False, size=size)
+            if train:
+                dataset = torch.load(
+                    os.path.join(MULTI_COVID19_DIR, f"train_{size}.pt")
+                )
+            else:
+                dataset = torch.load(
+                    os.path.join(MULTI_COVID19_DIR, f"test_{size}.pt")
+                )
         if train:
             path = os.path.join(MULTI_COVID19_DIR, "data/train")
             self.annotations = read_metadata(
