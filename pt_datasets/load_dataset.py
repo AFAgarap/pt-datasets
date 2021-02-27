bf4bc765a3470f647d30d4dafb0f68986c41e297
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-02-27 21:38:45 +0800

    add docstring for data loader

--- pt_datasets/load_dataset.py
+++ pt_datasets/load_dataset.py
@@ -260,6 +260,18 @@ def load_dataset(
 def load_mnist(
     data_folder: str = "~/datasets", augment: bool = False, normalize: bool = False
 ) -> Tuple[torch.utils.data.Dataset, torch.utils.data.Dataset]:
+    """
+    Loads the MNIST training and test datasets.
+
+    Parameters
+    ----------
+    data_folder: str
+        The path to the folder for the datasets.
+    augment: bool
+        Whether to perform data augmentation or not.
+    normalize: bool
+        Whether to normalize data or not.
+    """
     train_transform = torchvision.transforms.Compose(
         [torchvision.transforms.ToTensor()]
     )
