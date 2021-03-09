cbeaf2a663dd5b3902373a2a453892bccfd2765d
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-03-09 15:50:28 +0800

    deps: bump torch to v1.8 and torchvision to v0.9

--- setup.py
+++ setup.py
@@ -33,9 +33,9 @@ setup(
     long_description_content_type="text/markdown",
     install_requires=[
         "numpy==1.20.0",
-        "torchvision==0.8.2",
+        "torchvision==0.9.0",
         "umap_learn==0.4.6",
-        "torch==1.7.1",
+        "torch==1.8.0",
         "MulticoreTSNE==0.1",
         "scikit_learn==0.23.2",
         "gdown==3.12.2",
