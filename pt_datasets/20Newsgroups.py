d66e99d6108f76e853d3a9cbc8e4175d003a4d28
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-04-03 21:25:57 +0800

    preprocess features and labels

--- pt_datasets/20Newsgroups.py
+++ pt_datasets/20Newsgroups.py
@@ -1,6 +1,8 @@
 from sklearn.datasets import fetch_20newsgroups
 import torch
 
+from pt_datasets.utils import preprocess_data
+
 
 class TwentyNewsgroups(torch.utils.data.Dataset):
     def __init__(self, vectorizer: str = "tfidf", return_vectorizer: bool = False):
@@ -19,5 +21,11 @@ class TwentyNewsgroups(torch.utils.data.Dataset):
         )
         self.test_set = fetch_20newsgroups(
             subset="test", remove=("headers", "footers", "quotes")
+        )
+        (train_features, train_labels) = preprocess_data(
+            self.train_set.data, self.train_set.target
+        )
+        (test_features, test_labels) = preprocess_data(
+            self.test_set.data, self.test_set.target
         )
         self.classes = self.train_set.target_names
