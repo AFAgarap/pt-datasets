b76f951ace17b92777a4a04bc676492ed107d0e2
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-04-03 20:55:27 +0800

    add params for constructor

--- pt_datasets/20Newsgroups.py
+++ pt_datasets/20Newsgroups.py
@@ -3,7 +3,7 @@ import torch
 
 
 class TwentyNewsgroups(torch.utils.data.Dataset):
-    def __init__(self):
+    def __init__(self, vectorizer: str = "tfidf", return_vectorizer: bool = False):
         self.train_set = fetch_20newsgroups(
             subset="train", remove=("headers", "foooters", "quotes")
         )
