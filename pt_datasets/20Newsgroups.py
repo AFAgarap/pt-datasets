bda94fe85515db307dcef58bea966dd853a1cecf
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-04-03 20:53:41 +0800

    load 20newsgroups

--- pt_datasets/20Newsgroups.py
+++ pt_datasets/20Newsgroups.py
@@ -1,6 +1,12 @@
+from sklearn.datasets import fetch_20newsgroups
 import torch
 
 
 class TwentyNewsgroups(torch.utils.data.Dataset):
     def __init__(self):
-        pass
+        self.train_set = fetch_20newsgroups(
+            subset="train", remove=("headers", "foooters", "quotes")
+        )
+        self.test_set = fetch_20newsgroups(
+            subset="test", remove=("headers", "footers", "quotes")
+        )
