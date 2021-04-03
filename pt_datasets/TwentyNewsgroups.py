f99205437d2e9aa92976a79c99947a7be4709503
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-04-03 21:35:09 +0800

    load training and test sets separately

--- pt_datasets/TwentyNewsgroups.py
+++ pt_datasets/TwentyNewsgroups.py
@@ -23,25 +23,27 @@ class TwentyNewsgroups(torch.utils.data.Dataset):
         return_vectorizer: bool
             Whether to return the vectorizer object or not.
         """
-        self.train_set = fetch_20newsgroups(
-            subset="train", remove=("headers", "foooters", "quotes")
-        )
-        self.test_set = fetch_20newsgroups(
-            subset="test", remove=("headers", "footers", "quotes")
-        )
-        (train_features, train_labels) = preprocess_data(
-            self.train_set.data, self.train_set.target
-        )
-        (test_features, test_labels) = preprocess_data(
-            self.test_set.data, self.test_set.target
-        )
-        if return_vectorizer:
-            train_features, vectorizer = vectorize_text(
-                train_features, vectorizer=vectorizer
+        if train:
+            self.train_set = fetch_20newsgroups(
+                subset="train", remove=("headers", "foooters", "quotes")
             )
+            (train_features, train_labels) = preprocess_data(
+                self.train_set.data, self.train_set.target
+            )
+            if return_vectorizer:
+                train_features, vectorizer = vectorize_text(
+                    train_features, vectorizer=vectorizer
+                )
+            else:
+                train_features = vectorize_text(train_features, vectorizer=vectorizer)
         else:
-            train_features = vectorize_text(train_features, vectorizer=vectorizer)
-        test_features = vectorize_text(test_features, vectorizer=vectorizer)
+            self.test_set = fetch_20newsgroups(
+                subset="test", remove=("headers", "footers", "quotes")
+            )
+            (test_features, test_labels) = preprocess_data(
+                self.test_set.data, self.test_set.target
+            )
+            test_features = vectorize_text(test_features, vectorizer=vectorizer)
         self.train_features = train_features
         self.train_labels = train_labels
         self.test_features = test_features
