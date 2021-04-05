e8007220577c510a8433c393c97fe26601fa86e0
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-04-05 20:55:25 +0800

    add params for constructor

--- pt_datasets/AGNews.py
+++ pt_datasets/AGNews.py
@@ -2,5 +2,10 @@ import torch
 
 
 class AGNews(torch.utils.data.Dataset):
-    def __init__(self):
+    def __init__(
+        self,
+        train: bool = True,
+        vectorizer: str = "tfidf",
+        return_vectorizer: bool = False,
+    ):
         pass
