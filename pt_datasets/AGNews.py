02bb2794918ad779f065843fbce87906fb53623f
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-04-05 20:57:14 +0800

    add constructor docstring

--- pt_datasets/AGNews.py
+++ pt_datasets/AGNews.py
@@ -8,4 +8,16 @@ class AGNews(torch.utils.data.Dataset):
         vectorizer: str = "tfidf",
         return_vectorizer: bool = False,
     ):
+        """
+        Loads the AG News dataset.
+
+        Parameters
+        ----------
+        train: bool
+            Whether to load the training set or not.
+        vectorizer: str
+            The vectorizer to use, options: [tfidf (default) | ngrams]
+        return_vectorizer: bool
+            Whether to return the vectorizer object or not.
+        """
         pass
