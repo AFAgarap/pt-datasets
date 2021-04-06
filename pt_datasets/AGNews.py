a9af877eab7325f30f0c32f71ffcd3c763d42724
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-04-06 18:54:56 +0800

    add module header

--- pt_datasets/AGNews.py
+++ pt_datasets/AGNews.py
@@ -1,3 +1,19 @@
+# PyTorch Datasets utility repository
+# Copyright (C) 2020  Abien Fred Agarap
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Class for AG News dataset"""
 import os
 from pathlib import Path
 from typing import Any, Tuple
@@ -5,6 +21,8 @@ from typing import Any, Tuple
 import torch
 
 from pt_datasets.utils import read_data, preprocess_data, vectorize_text
+
+__author__ = "Abien Fred Agarap"
 
 
 class AGNews(torch.utils.data.Dataset):
