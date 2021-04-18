ef76997685d6c237c54efbd8e3a2ffdbe75f5456
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-04-18 19:35:41 +0800

    fix: convert GDrive link for MalImg to str

--- pt_datasets/load_dataset.py
+++ pt_datasets/load_dataset.py
@@ -455,7 +455,7 @@ def load_malimg(
         test_dataset
             The test set of MalImg dataset.
     """
-    download_url = ("https://drive.google.com/uc?id=1Y6Ha5Jir8EI726KdwAKWHVU-oWmPqNDT",)
+    download_url = "https://drive.google.com/uc?id=1Y6Ha5Jir8EI726KdwAKWHVU-oWmPqNDT"
     malimg_filename = "malimg_dataset_32x32.npy"
     dataset_path = os.path.join(str(Path.home()), "datasets")
     if not os.path.exists(dataset_path):
