21e9d4f761d9490e13296ca659ca84961dd612d9
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-04-26 17:26:10 +0800

    add test function for emnist dataset and data loader

--- tests/test_datasets.py
+++ tests/test_datasets.py
@@ -1,5 +1,5 @@
 import numpy as np
-from pt_datasets import create_dataloader
+from pt_datasets import create_dataloader, load_dataset
 from pt_datasets.load_dataset import load_mnist, load_fashion_mnist
 
 
@@ -42,3 +42,20 @@ def test_load_fashion_mnist():
     assert batch_labels.shape == (32,)
     assert batch_features.numpy().min() == 0.0
     assert batch_features.numpy().max() == 1.0
+
+
+def test_emnist():
+    train_data, test_data = load_dataset("emnist")
+    assert (len(train_data), len(test_data)) == (112_800, 18_800)
+    assert train_data.data.numpy().min() == 0
+    assert train_data.data.numpy().max() == 255
+    assert test_data.data.numpy().min() == 0
+    assert test_data.data.numpy().max() == 255
+    train_loader = create_dataloader(train_data, batch_size=32)
+    for index, (batch_features, batch_labels) in enumerate(train_loader):
+        if index == 1:
+            break
+    assert batch_features.shape == (32, 1, 28, 28)
+    assert batch_labels.shape == (32,)
+    assert np.isclose(batch_features.numpy().min(), -0.42421296)
+    assert np.isclose(batch_features.numpy().max(), 2.8214867)
