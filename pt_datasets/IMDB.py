a509e1ecf78cfb4ef65174a52fadff7c39e47842
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-05-07 13:36:38 +0800

    prepare class

---
+++ pt_datasets/IMDB.py
@@ -0,0 +1,12 @@
+import torch
+
+
+class IMDB(torch.utils.data.Dataset):
+    def __init__(self):
+        super().__init__()
+
+    def __getitem__(self):
+        pass
+
+    def __len__(self):
+        pass
