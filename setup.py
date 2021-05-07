60534863d5e3f48d1534627b7e8f95c8f321674e
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-05-07 14:06:59 +0800

    add module header for setup file

--- setup.py
+++ setup.py
@@ -1,9 +1,27 @@
+# PyTorch Datasets utility repository
+# Copyright (C) 2020  Abien Fred Agarap
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""Setup file for pt-datasets"""
 import os
 from os import path
 from pathlib import Path
 from setuptools import setup
 
 import gdown
+
+__author__ = "Abien Fred Agarap"
 
 
 this_directory = path.abspath(path.dirname(__file__))
