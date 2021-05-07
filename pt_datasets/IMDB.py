6d81244e7dbdccb478c1269a46307418941e67aa
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-05-07 18:14:56 +0800

    define __len__

--- pt_datasets/IMDB.py
+++ pt_datasets/IMDB.py
@@ -68,7 +68,7 @@ class IMDB(torch.utils.data.Dataset):
         return (text, target)
 
     def __len__(self):
-        pass
+        return len(self.data)
 
     @staticmethod
     def load_data(data: str = "data/IMDB Dataset.csv"):
