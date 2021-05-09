c5fabbc8f30f389238443dce1d9d12eb6cbdae9a
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-05-09 14:28:58 +0800

    chore: bump package version to v0.12.1

--- pt_datasets/__init__.py
+++ pt_datasets/__init__.py
@@ -20,7 +20,7 @@ from .encode_features import encode_features
 from .load_dataset import load_dataset
 
 __author__ = "Abien Fred Agarap"
-__version__ = "0.12.0"
+__version__ = "0.12.1"
 
 
 def list_datasets() -> str:
