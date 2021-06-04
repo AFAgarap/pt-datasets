63313b59bb38bcf2f27e1cf706f0b71d7635cf51
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-06-04 14:35:49 +0800

    add constructor param and docstring

--- pt_datasets/WDBC.py
+++ pt_datasets/WDBC.py
@@ -4,7 +4,15 @@ import torch
 
 
 class WDBC(torch.utils.data.Dataset):
-    def __init__(self):
+    def __init__(self, train: bool = True):
+        """
+        Loads the WDBC dataset.
+
+        Parameter
+        ---------
+        train: bool
+            Whether to load the training set or not.
+        """
         super().__init__()
 
     def __getitem__(self, index: int) -> Tuple[Any, Any]:
