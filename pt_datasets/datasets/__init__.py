ef19bc2e605f1517108c011d26e2f0e7b5bfedf4
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-06-16 17:11:31 +0800

    import dataset classes at datasets subpackage level

---
+++ pt_datasets/datasets/__init__.py
@@ -0,0 +1,9 @@
+from pt_datasets.datasets.AGNews import AGNews
+from pt_datasets.datasets.COVID19Dataset import (
+    BinaryCOVID19Dataset,
+    MultiCOVID19Dataset,
+)
+from pt_datasets.datasets.IMDB import IMDB
+from pt_datasets.datasets.TwentyNewsgroups import TwentyNewsgroups
+from pt_datasets.datasets.WDBC import WDBC
+from pt_datasets.datasets.Yelp import Yelp
